43debec10b212248a67fbfc8e1de881aebe53167
Ferdiansyah <ferdif9996@gmail.com>
2021-05-17 11:10:21 +0700

    config activated vue devtools detected

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
 import router from './router'
-import {auth} from '@/firebase'
+import {auth} from '@/firebase';
 
 var app
 auth.onAuthStateChanged(() => {
 	if(!app) {
 		app = createApp(App).use(router).mount('#app')
+		window.postMessage({
+			devtoolsEnabled: true,
+			vueDetected: true
+		})
 	}
 })
