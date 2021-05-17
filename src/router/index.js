c98f34c6bf7644562290751f572eb7df82d435c7
Ferdiansyah <ferdif9996@gmail.com>
2021-05-17 09:18:24 +0700

    new router & beforeEach authentication

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import {auth} from '@/firebase'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
   {
     path: '/about',
@@ -20,6 +30,16 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+  if(requiresAuth && !auth.currentUser){
+    next('/login')
+  }
+  else{
+    next()
+  }
 })
 
 export default router
